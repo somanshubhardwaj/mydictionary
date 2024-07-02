313095b2598dfd0819b6fd23e11ebc84c128d529
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-07-02 11:41:06 +0530

    ui fixes

--- src/App.tsx
+++ src/App.tsx
@@ -16,81 +16,73 @@ function App() {
     }
   };
 
-  return (
-    <>
-      <div className="App p-6">
-        <header className="App-header">
-          <h1 className="text-3xl font-bold mb-4">Dictionary</h1>
+    return (
+      <div className="min-h-screen p-6">
+        <nav className="font-extrabold text-3xl">MyDictionary</nav>
+        <div className="divider"></div>
+        <section className="flex gap-2">
           <input
             type="text"
-            placeholder="Search"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            className="border border-gray-300 rounded-md px-4 py-2 mb-4 mr-4"
           />
-          <button
-            onClick={searchword}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-          >
-            Search
-          </button>
-          <div className="flex md:flex-row flex-col">
-            {meanings.map((meaning: any, index: any) => {
-              return (
-                <div key={index} className="mb-4">
-                  <h2 className="text-2xl font-bold mb-2">{meaning.word}</h2>
-                  <div>
-                    {meaning.meanings.map((mean: any, index: number) => {
-                      return (
-                        <>
-                          <div key={index} className="my-2">
-                            <h3 className="text-xl font-bold">
-                              {mean.partOfSpeech}
-                            </h3>
-                            {mean.definitions.map((def: any, index: number) => {
-                              return (
-                                <div key={index} className="mb-2">
-                                  <h4 className="font-bold">
-                                    {def.definition}
-                                  </h4>
-                                  <h5>{def.example}</h5>
-                                </div>
-                              );
-                            })}
-                          </div>
-
-                          <h6 className="mb-2">{mean.usage}</h6>
-                        </>
-                      );
-                    })}
-                  </div>
-                  {meaning.phonetics.map((phon: any, index: number) => {
+          <button className="btn btn-primary" onClick={searchword}>Search</button>
+        </section>
+        <section className="py-2">
+          {meanings.map((meaning: any, index: any) => {
+            return (
+              <div className="" key={index}>
+                <h2 className="text-2xl font-bold mb-2">{meaning.word}</h2>
+                <div className="flex flex-col sm:flex-row gap-3">
+                  {meaning.meanings.map((mean: any, index: number) => {
                     return (
-                      <div key={index} className="my-2">
-                        <h3 className="text-xl font-bold">{phon.text}</h3>
-                        <audio controls className="my-2">
-                          <source src={phon.audio} type="audio/mpeg" />
-                        </audio>
+                      <div key={index} className="my-2 glass p-3 text-white rounded-lg">
+                        <h3 className="text-2xl font-bold">
+                          {mean.partOfSpeech}
+                        </h3>
+                        {mean.definitions.map((def: any, index: number) => {
+                          return (
+                            <div key={index} className="mb-2">
+                              <h4 className="font-bold">- {def.definition}</h4>
+                              <h5>{def.example}</h5>
+                            </div>
+                          );
+                        })}
                       </div>
                     );
                   })}
-                  <div>
-                    Source Urls:
-                    <a
-                      href={meaning.sourceUrls}
-                      className="text-blue-500 hover:underline"
-                    >
-                      {meaning.sourceUrls}
-                    </a>
-                  </div>
                 </div>
-              );
-            })}
-          </div>
-        </header>
+                <div className="flex flex-wrap items-end gap-3">
+                {meaning.phonetics.map((phon: any, index: number) => {
+                  return (
+                    <div key={index} className="my-2">
+                      <h3 className="text-xl font-bold">{phon.text}</h3>
+                      <audio controls className="mt-2">
+                        <source src={phon.audio} type="audio/mpeg" />
+                      </audio>
+                    </div>
+                  );
+                })}</div>
+                <div>
+                  Source Urls:
+                  <a
+                    href={meaning.sourceUrls}
+                    className="text-blue-500 hover:underline"
+                  >
+                    {meaning.sourceUrls}
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </section>
       </div>
-    </>
-  );
+    );
+  
+
+ 
 }
 
 export default App;
